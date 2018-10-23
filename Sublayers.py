37bf49224ccc0ab5a2c8cdb2c330ccd76628e57a
SamLynnEvans <samuellynnevans@gmail.com>
2018-10-23 15:32:03 +0100

    Update Sublayers.py

--- Sublayers.py
+++ Sublayers.py
@@ -9,10 +9,13 @@ class Norm(nn.Module):
         super().__init__()
     
         self.size = d_model
+        
         # create two learnable parameters to calibrate normalisation
         self.alpha = nn.Parameter(torch.ones(self.size))
         self.bias = nn.Parameter(torch.zeros(self.size))
+        
         self.eps = eps
+    
     def forward(self, x):
         norm = self.alpha * (x - x.mean(dim=-1, keepdim=True)) \
         / (x.std(dim=-1, keepdim=True) + self.eps) + self.bias
@@ -22,12 +25,10 @@ def attention(q, k, v, d_k, mask=None, dropout=None):
     
     scores = torch.matmul(q, k.transpose(-2, -1)) /  math.sqrt(d_k)
     
-
     if mask is not None:
         mask = mask.unsqueeze(1)
         scores = scores.masked_fill(mask == 0, -1e9)
     
-
     scores = F.softmax(scores, dim=-1)
     
     if dropout is not None:
@@ -47,6 +48,7 @@ class MultiHeadAttention(nn.Module):
         self.q_linear = nn.Linear(d_model, d_model)
         self.v_linear = nn.Linear(d_model, d_model)
         self.k_linear = nn.Linear(d_model, d_model)
+        
         self.dropout = nn.Dropout(dropout)
         self.out = nn.Linear(d_model, d_model)
     
@@ -58,9 +60,8 @@ class MultiHeadAttention(nn.Module):
         k = self.k_linear(k).view(bs, -1, self.h, self.d_k)
         q = self.q_linear(q).view(bs, -1, self.h, self.d_k)
         v = self.v_linear(v).view(bs, -1, self.h, self.d_k)
+        
         # transpose to get dimensions bs * N * sl * d_model
-       
-       
         k = k.transpose(1,2)
         q = q.transpose(1,2)
         v = v.transpose(1,2)
@@ -78,10 +79,12 @@ class MultiHeadAttention(nn.Module):
 class FeedForward(nn.Module):
     def __init__(self, d_model, d_ff=2048, dropout = 0.1):
         super().__init__() 
+    
         # We set d_ff as a default to 2048
         self.linear_1 = nn.Linear(d_model, d_ff)
         self.dropout = nn.Dropout(dropout)
         self.linear_2 = nn.Linear(d_ff, d_model)
+    
     def forward(self, x):
         x = self.dropout(F.relu(self.linear_1(x)))
         x = self.linear_2(x)
